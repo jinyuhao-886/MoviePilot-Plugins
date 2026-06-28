7e9bf03ce49c6ca60fe2ec267ba69e5da1ef4ec4
jyh <jinyuhao-886@users.noreply.github.com>
2026-06-28 13:04:43 +0800

    chore: bump to v1.6.5

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -43,7 +43,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.6.4"
+    plugin_version = "1.6.5"
     # 插件作者
     plugin_author = "jinyuhao-886"
     # 作者主页
