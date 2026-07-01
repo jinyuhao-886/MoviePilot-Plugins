9c7797b40ac4368e890c45e5f9b502e327fc5689
jyh <jinyuhao-886@users.noreply.github.com>
2026-07-01 18:44:59 +0800

    chore: 插件名去掉版本号

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -37,7 +37,7 @@ class P115StrgmSub(_PluginBase):
     """115网盘订阅追更插件"""
 
     # 插件名称
-    plugin_name = "115网盘订阅追更魔改版v1.6.96"
+    plugin_name = "115网盘订阅追更魔改版"
     # 插件描述
     plugin_desc = "结合MoviePilot订阅功能，自动搜索115网盘资源并转存缺失的电影和剧集。"
     # 插件图标
