33a906cfeb327b486948c7773c10629ab1b3c848
jyh <jinyuhao-886@users.noreply.github.com>
2026-07-16 14:07:59 +0800

    v1.7.1: TG频道搜索+自动转发+集数守护

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -47,7 +47,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.8.0"
+    plugin_version = "1.7.1"
     # 插件作者
     plugin_author = "jinyuhao-886"
     # 作者主页
